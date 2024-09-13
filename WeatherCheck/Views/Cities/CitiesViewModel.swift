ab0cfe33ea2ce9ff61706ff2ffaf07f445f0b511
Javier Manzo <javier.r.manzo@gmail.com>
2024-09-13 13:04:17 -0300

    Add calendar event the first time a city is added

--- WeatherCheck/Views/Cities/CitiesViewModel.swift
+++ WeatherCheck/Views/Cities/CitiesViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import LocationData
 import WeatherData
+import Reminder
 
 protocol CitiesViewModelDelegate: AnyObject {
     func reloadTableView()
@@ -18,55 +19,55 @@ final class CitiesViewModel {
     private let repository: WeatherRepositoryProtocol
     var currentCity: CityModel?
     private var cities: [CityModel] = []
-
+    
     var sortedCities: [CityModel] {
         Array(cities).sorted(by: { $0.createdAt < $1.createdAt })
     }
-
+    
     init(repository: WeatherRepositoryProtocol = WeatherRepository()) {
         self.repository = repository
     }
-
+    
     func fetchData() {
         getCurrentCity()
         fetchSavedCities()
     }
-
+    
     func updateCitiesWeather() {
         requestWeatherAndUpdateCurrentCity()
-
+        
         for city in cities {
             requestWeatherAndUpdateCity(city)
         }
     }
-
+    
     private func fetchSavedCities() {
         cities = repository.fetchSavedCities()
-
+        
         delegate?.reloadTableView()
-
+        
         for city in cities {
             requestWeatherAndUpdateCity(city)
         }
     }
-
+    
     private func getCurrentCity() {
         Task { @MainActor in
             let status = await LocationData.shared.checkLocationAuthorizationStatus()
-
+            
             switch status {
             case .authorizedWhenInUse, .authorizedAlways:
                 do {
                     let location = try await LocationData.shared.requestCurrentLocation()
-
+                    
                     if let cityName = location.name {
                         let city = CityModel(id: location.id, name: cityName, latitude: location.latitude, longitude: location.longitude, createdAt: Date().timeIntervalSince1970)
-
+                        
                         await MainActor.run {
                             currentCity = city
                             delegate?.reloadTableView()
                         }
-
+                        
                         requestWeatherAndUpdateCurrentCity()
                     }
                 } catch {
@@ -77,7 +78,7 @@ final class CitiesViewModel {
             }
         }
     }
-
+    
     func requestWeatherAndUpdateCurrentCity() {
         Task {
             if let currentCity = self.currentCity,
@@ -90,7 +91,7 @@ final class CitiesViewModel {
             }
         }
     }
-
+    
     func requestWeatherAndUpdateCity(_ city: CityModel) {
         Task {
             if let weather = await requestWeather(city: city) {
@@ -103,10 +104,10 @@ final class CitiesViewModel {
             }
         }
     }
-
+    
     private func requestWeather(city: CityModel) async -> WeatherResponseModel? {
         let response = await repository.requestWeather(latitude: city.latitude, longitude: city.longitude, details: false)
-
+        
         switch response {
         case .success(let weather):
             return weather
@@ -117,12 +118,12 @@ final class CitiesViewModel {
             return nil
         }
     }
-
+    
     private func updateCity(_ city: CityModel, weather: WeatherResponseModel) -> CityModel {
         let updatedCity = CityModel(id: city.id, name: city.name, latitude: city.latitude, longitude: city.longitude, createdAt: city.createdAt, weather: weather)
         return updatedCity
     }
-
+    
     private func replaceOrAddCity(_ city: CityModel) {
         if let index = cities.firstIndex(where: { $0.id == city.id }) {
             cities[index] = city
@@ -130,14 +131,37 @@ final class CitiesViewModel {
             cities.append(city)
         }
     }
+    
+    private func addCalendarEvent() {
+        Task {
+            let response = await Reminder.checkCalendarAuthorizationStatus()
+            
+            switch response {
+            case .success(let status):
+                if status == .authorized || status == .restricted {
+                    do {
+                        try Reminder.createCalendarEvent(eventTitle: "Weather Check", hour: 9, minute: 0, durationInMinutes: 15, daily: true)
+                    } catch {
+                        print(error)
+                    }
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
 // MARK: - CitiesViewModelDelegate
 extension CitiesViewModel: AddCityDelegate {
     func didAddCity(_ city: CityModel) {
+        if cities.isEmpty {
+            addCalendarEvent()
+        }
+        
         replaceOrAddCity(city)
         delegate?.reloadTableView()
-
+        
         repository.saveOrUpdateCity(city)
         requestWeatherAndUpdateCity(city)
     }
